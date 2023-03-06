5b9723ee5b9e2221fe8c451edeea65f6f53e13ba
MRcoin2 <rosowskiszymon@gmail.com>
2023-03-06 12:07:26 +0100

    small changes to form widget

--- lib/features/database_comunication/new_beer_form_widget.dart
+++ lib/features/database_comunication/new_beer_form_widget.dart
@@ -14,11 +14,14 @@ class _AddBeerFormState extends State<AddBeerForm> {
   final TextEditingController _breweryController = TextEditingController();
   final TextEditingController _styleController = TextEditingController();
   final TextEditingController _alcoholContentController =
-  TextEditingController();
+      TextEditingController();
 
   void _submitForm() async {
     try {
-      await FirebaseFirestore.instance.collection('beers').doc(_barcodeController.text).set({
+      await FirebaseFirestore.instance
+          .collection('beers')
+          .doc(_barcodeController.text)
+          .set({
         'name': _nameController.text,
         'brewery': _breweryController.text,
         'style': _styleController.text,
@@ -41,87 +44,101 @@ class _AddBeerFormState extends State<AddBeerForm> {
     }
   }
 
+  Widget _buildTextFormField(TextEditingController controller, String label,
+      String? Function(String?) validator,
+      {TextInputType keyboardType = TextInputType.text}) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextFormField(
+        controller: controller,
+        decoration: InputDecoration(
+          labelText: label,
+        ),
+        keyboardType: keyboardType,
+        validator: validator,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          TextFormField(
-            controller: _barcodeController,
-            decoration: InputDecoration(
-              labelText: 'Barcode',
-            ),
-            validator: (value) {
-              if (value!.isEmpty) {
-                return 'Please enter a barcode';
-              }
-              return null;
-            },
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add Beer'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              _buildTextFormField(
+                _barcodeController,
+                'Barcode',
+                (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter a barcode';
+                  }
+                  return null;
+                },
+              ),
+              _buildTextFormField(
+                _nameController,
+                'Name',
+                (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter a name';
+                  }
+                  return null;
+                },
+              ),
+              _buildTextFormField(
+                _breweryController,
+                'Brewery',
+                (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter a brewery';
+                  }
+                  return null;
+                },
+              ),
+              _buildTextFormField(
+                _styleController,
+                'Style',
+                (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter a style';
+                  }
+                  return null;
+                },
+              ),
+              _buildTextFormField(
+                _alcoholContentController,
+                'Alcohol Content',
+                (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter an alcohol content';
+                  }
+                  if (double.tryParse(value) == null) {
+                    return 'Please enter a valid number';
+                  }
+                  return null;
+                },
+                keyboardType: TextInputType.number,
+              ),
+              SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    _submitForm();
+                  }
+                },
+                child: Text('Add Beer'),
+              ),
+            ],
           ),
-          TextFormField(
-            controller: _nameController,
-            decoration: InputDecoration(
-              labelText: 'Name',
-            ),
-            validator: (value) {
-              if (value!.isEmpty) {
-                return 'Please enter a name';
-              }
-              return null;
-            },
-          ),
-          TextFormField(
-            controller: _breweryController,
-            decoration: InputDecoration(
-              labelText: 'Brewery',
-            ),
-            validator: (value) {
-              if (value!.isEmpty) {
-                return 'Please enter a brewery';
-              }
-              return null;
-            },
-          ),
-          TextFormField(
-            controller: _styleController,
-            decoration: InputDecoration(
-              labelText: 'Style',
-            ),
-            validator: (value) {
-              if (value!.isEmpty) {
-                return 'Please enter a style';
-              }
-              return null;
-            },
-          ),
-          TextFormField(
-            controller: _alcoholContentController,
-            decoration: InputDecoration(
-              labelText: 'Alcohol Content',
-            ),
-            keyboardType: TextInputType.number,
-            validator: (value) {
-              if (value!.isEmpty) {
-                return 'Please enter an alcohol content';
-              }
-              if (double.tryParse(value) == null) {
-                return 'Please enter a valid number';
-              }
-              return null;
-            },
-          ),
-          SizedBox(height: 16.0),
-          ElevatedButton(
-            onPressed: () {
-              if (_formKey.currentState!.validate()) {
-                _submitForm();
-              }
-            },
-            child: Text('Add Beer'),
-          ),
-        ],
+        ),
       ),
     );
   }
